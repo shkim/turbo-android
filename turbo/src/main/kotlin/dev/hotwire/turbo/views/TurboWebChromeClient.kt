050513c1c1a3018760618676e4479b6d2aabbf82
Jay Ohms <jay.ohms@gmail.com>
2021-02-22 18:05:19 -0500

    Support retrieving the target="_blank" anchor url through HitTestResult

--- turbo/src/main/kotlin/dev/hotwire/turbo/views/TurboWebChromeClient.kt
+++ turbo/src/main/kotlin/dev/hotwire/turbo/views/TurboWebChromeClient.kt
@@ -5,7 +5,9 @@ import android.os.Message
 import android.webkit.ValueCallback
 import android.webkit.WebChromeClient
 import android.webkit.WebView
+import android.webkit.WebView.HitTestResult
 import dev.hotwire.turbo.session.TurboSession
+import dev.hotwire.turbo.util.TurboLog
 import dev.hotwire.turbo.util.toJson
 import dev.hotwire.turbo.visit.TurboVisitOptions
 
@@ -22,16 +24,29 @@ open class TurboWebChromeClient(val session: TurboSession) : WebChromeClient() {
     }
 
     override fun onCreateWindow(webView: WebView, isDialog: Boolean, isUserGesture: Boolean, resultMsg: Message?): Boolean {
+        val location = hitTestResult(webView)
+            ?: messageResult(webView)
+            ?: return false
+
+        session.visitProposedToLocation(
+            location = location,
+            optionsJson = TurboVisitOptions().toJson()
+        )
+
+        return false
+    }
+
+    private fun hitTestResult(webView: WebView): String? {
+        return when (webView.hitTestResult.type) {
+            HitTestResult.SRC_ANCHOR_TYPE -> webView.hitTestResult.extra
+            else -> null
+        }
+    }
+
+    private fun messageResult(webView: WebView): String? {
         val message = webView.handler.obtainMessage()
         webView.requestFocusNodeHref(message)
 
-        message.data.getString("url")?.let {
-            session.visitProposedToLocation(
-                location = it,
-                optionsJson = TurboVisitOptions().toJson()
-            )
-        }
-
-        return false
+        return message.data.getString("url")
     }
 }
